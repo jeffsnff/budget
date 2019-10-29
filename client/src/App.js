991a3d24217deac293d6a31ee270849036636ed9
Jeffrey Seneff <jeffsnff@gmail.com>
2019-10-29 10:58:51 -0600

    my first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+/** The doStuff function
+ * @function - doStuff
+ * @argument Number 
+ * @argument String
+ * @return Object
+ */
+
+function App(){
+    return(
+        <h1>Hello World</h1>
+    )
+}
+export default App
+
---
+++ server.js
@@ -0,0 +1,31 @@
+
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const PORT = 7000
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+// middleware for every request
+app.use(express.json())
+app.use(morgan('dev')) // gives updates while using CRUD
+
+// DB collection
+mongoose.connect('mongodb://localhost:27017/budget', {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useCreateIndex: false,
+    useUnifiedTopology: true
+}, () => console.log('Connected to Budget DB'))
+
+// routes
+app.use('/auth', require('./routes/authRouter.js'))
+
+// Global Error Handling
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({errMsg: err.message})
+})
+app.listen(PORT, () => {
+    console.log(`Server running on Port : ${PORT}`)
+})
