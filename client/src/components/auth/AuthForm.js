b93eaa0038c4106e5d55cedf3d1639c3af0f704b
Jeffrey <jeffsnff@gmail.com>
2020-04-10 11:29:07 -0700

    Redesigned Login/Signup form

--- client/src/components/auth/AuthForm.js
+++ client/src/components/auth/AuthForm.js
@@ -1,24 +1,25 @@
 import React from 'react'
-
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
 function AuthForm(props){
     const { handleChange, handleSubmit, inputs: { username, password }, btnText } = props
     return (
-        <form className="authForm" onSubmit={handleSubmit}>
-            <input 
-                type="text" 
-                name="username" 
-                value={username} 
-                onChange={handleChange} 
-                placeholder="username"/>
-                <input 
-                type="text" 
-                name="password" 
-                value={password} 
-                onChange={handleChange} 
-                placeholder="password"/>
-
-            <button>{btnText}</button>
-        </form>
+      <MDBContainer className="col-xl-12">
+        <MDBRow className="row d-flex justify-content-center">
+          <MDBCol className="col-xl-6">
+            <form onSubmit={handleSubmit}>
+              <p className="h5 text-center mb-4">Sign in</p>
+              <div className="grey-text">
+                <MDBInput name="username" onChange={handleChange} label="Type your email" icon="envelope" group type="email" validate value={username}error="wrong"
+                  success="right" />
+                <MDBInput name="password" onChange={handleChange} label="Type your password" icon="lock" group type="password" validate value={password} />
+              </div>
+              <div className="text-center">
+                <MDBBtn type="submit">{btnText}</MDBBtn>
+              </div>
+            </form>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
     )
 }
 
