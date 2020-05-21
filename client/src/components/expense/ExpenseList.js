02b41b6019adc71add36e7ccea9949afe72d8849
Jeffrey <jeffsnff@gmail.com>
2020-05-20 17:52:09 -0700

    start of sorting data in table

--- client/src/components/expense/ExpenseList.js
+++ client/src/components/expense/ExpenseList.js
@@ -10,32 +10,59 @@ function ExpenseList(props){
     const { getBankExpense, expenses, getAllAccounts, accounts } = useContext(BankContext)
     let currentBank = {}
 
+    // Updates webpage with new account information after a new expense is added
     useEffect(() => {
         getBankExpense(accId)
         getAllAccounts()
     },[])
 
+    
+
+    // returns the account that the account id matches to for the
+    // bank name at the top of webpage
     const result = accounts ? accounts.filter(account => {
         return account._id === accId
     }) : []
     currentBank = result[0] ? result[0]: {}
 
+   
+
+    // Sorts expenses before they go into the mappedExpenses
     expenses.sort(function(a,b) {
-      let dateA = new Date(a.date), dateB = new Date(b.date)
+      let dateA = new Date(a.date)
+      let dateB = new Date(b.date)
       return dateA - dateB
     })
     
+    
 
-    const mappedExpenses = expenses.map(exp => 
+    // subtotal that is displayed at the top of web page
+    
+    
+    const sortFunction = (expenses) => {
+        expenses.sort(function(a,b) {
+        let dateA = new Date(a.date)
+        let dateB = new Date(b.date)
+        return dateB - dateA
+      })
+    }
+
+    // Maps expenses data to a new row and expense
+    let mappedExpenses;
+    const mapExpenses = (expenses) => {
+      mappedExpenses = expenses.map(exp => 
         <Expense  key={exp._id} {...exp} />
-    )
+      )
+    }
+    // const mappedExpenses = expenses.map(exp => 
+    //     <Expense  key={exp._id} {...exp} />
+    // )
 
-
+    mapExpenses(expenses)
     let subtotal = 0
     for(let i=0; i<mappedExpenses.length; i++){
         subtotal = subtotal + mappedExpenses[i].props.amount
     }
-
 
     return(
         <>
@@ -49,7 +76,7 @@ function ExpenseList(props){
             <MDBTable>
               <MDBTableHead>
                 <tr>
-                  <th>Date</th>
+                  <th onClick={() => sortFunction(expenses)}>Date</th>
                   <th>Payee</th>
                   <th>Catagory</th>
                   <th>Details</th>
